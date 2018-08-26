d267ae3f8a37a1169fe9873ccffaf5ab89489dbb
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-26 20:20:01 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,12 @@
 # require modules here
 require 'yaml'
+require 'pry'
 
 def load_library(file_path)
   lib = YAML.load_file(file_path)
   dictionary = {}
   lib.each_value do |term, symbols|
+    
     dictionary[symbols[0]] = symbols[1]
   end 
   dictionary

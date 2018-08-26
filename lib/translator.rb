8654b54c534aab6fdd61fa1e9043a6cf6107a14e
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-26 20:19:00 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,11 @@ require 'yaml'
 
 def load_library(file_path)
   lib = YAML.load_file(file_path)
-  lib.each do |term, symbols|
-    
+  dictionary = {}
+  lib.each_value do |term, symbols|
+    dictionary[symbols[0]] = symbols[1]
+  end 
+  dictionary
 end
 
 def get_japanese_emoticon

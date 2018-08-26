f8a52b43047a6f14279bc4e891f83c0e0a3f5b47
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-26 20:21:02 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require 'pry'
 def load_library(file_path)
   lib = YAML.load_file(file_path)
   dictionary = {}
-  lib.each_value do |term, symbols|
+  lib.each_value do |symbols|
     binding.pry
     dictionary[symbols[0]] = symbols[1]
   end 
